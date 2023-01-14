af617ba472a083a6ce927feae41c93c431656113
Massimiliano_Carrer <massimiliano.carrer@istitutovolterra.edu.it>
2023-01-14 07:54:55 +0000

    add old ClipRRect Component for border issue

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -32,134 +32,136 @@ class _LoginScreenState extends State<LoginScreen> {
           Image.asset('assets/images/Logo.png', scale: 3.5),
           Padding(padding: EdgeInsets.only(bottom: 24)),
 
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  color: utilities.mainColor,
-                  style: BorderStyle.solid,
-                  width: 8,
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: utilities.mainColor,
+                    style: BorderStyle.solid,
+                    width: 8,
+                  ),
+                  bottom: BorderSide(
+                    color: utilities.mainColor,
+                    style: BorderStyle.solid,
+                    width: 8,
+                  ),
                 ),
-                bottom: BorderSide(
-                  color: utilities.mainColor,
-                  style: BorderStyle.solid,
-                  width: 8,
-                ),
+                gradient: LinearGradient(
+                    colors: [utilities.lightBlue, utilities.purple],
+                    transform: GradientRotation(140)),
               ),
-              //borderRadius: BorderRadius.all(Radius.circular(34)),
-              gradient: LinearGradient(
-                  colors: [utilities.lightBlue, utilities.purple],
-                  transform: GradientRotation(140)),
-            ),
-            //margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20),
-            padding: const EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 20,
-            ),
-            child: Column(
-              children: [
-                TextField(
-                  style: TextStyle(
-                    color: utilities.white,
-                  ), // modifica stile testo che viene scritto nel TextField
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(
-                      // modifica stile label del TextField, quindi anche colore
+              //margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20),
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 20,
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    style: TextStyle(
                       color: utilities.white,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      // modifica stile bordi quando TextField é attivo, quindi in teoria quando é visibile
-                      borderSide: BorderSide(
-                        width: 2,
+                    ), // modifica stile testo che viene scritto nel TextField
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        // modifica stile label del TextField, quindi anche colore
+                        color: utilities.white,
                       ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      // modifica stile bordi quando TextField é selezionato
-                      borderSide: BorderSide(
-                        width: 3,
-                      ),
-                    ),
-                    fillColor: utilities.mainBackgroundColor,
-                    filled: true,
-                    labelText: 'Username',
-                  ),
-                  controller: username,
-                ),
-                const SizedBox(
-                  // simile a Spacer()
-                  height: 30,
-                ),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(
-                    color: utilities.white,
-                  ), // modifica stile testo che viene scritto nel TextField
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(
-                      // modifica stile label del TextField
-                      color: utilities.white,
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      // modifica stile bordi quando TextField é attivo, quindi in teoria quando é visibile
-                      borderSide: BorderSide(
-                        width: 2,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      // modifica stile bordi quando TextField é selezionato
-                      borderSide: BorderSide(
-                        width: 3,
-                      ),
-                    ),
-                    fillColor: utilities.mainBackgroundColor,
-                    filled: true,
-                    labelText: 'Password',
-                  ),
-                  controller: password,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(
-                    left: 20.0,
-                    right: 20.0,
-                    top: 20,
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: utilities.mainColor,
-                      padding: EdgeInsets.only(left: 50, right: 50),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                      ),
-                    ),
-                    onPressed: () {
-                      userAuthentication();
-                    },
-                    child: const Text('LOGIN',
-                        style: TextStyle(
-                          fontSize: 20,
+                      enabledBorder: OutlineInputBorder(
+                        // modifica stile bordi quando TextField é attivo, quindi in teoria quando é visibile
+                        borderSide: BorderSide(
+                          width: 2,
                         ),
-                        textAlign: TextAlign.center),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        // modifica stile bordi quando TextField é selezionato
+                        borderSide: BorderSide(
+                          width: 3,
+                        ),
+                      ),
+                      fillColor: utilities.mainBackgroundColor,
+                      filled: true,
+                      labelText: 'Username',
+                    ),
+                    controller: username,
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text("New here?",
-                      style: TextStyle(
-                        color: utilities.mainColor,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                        letterSpacing: 2,
-                      )),
-                ),
-                const Padding(padding: EdgeInsets.only(bottom: 10)),
-              ],
+                  const SizedBox(
+                    // simile a Spacer()
+                    height: 30,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    style: TextStyle(
+                      color: utilities.white,
+                    ), // modifica stile testo che viene scritto nel TextField
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        // modifica stile label del TextField
+                        color: utilities.white,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        // modifica stile bordi quando TextField é attivo, quindi in teoria quando é visibile
+                        borderSide: BorderSide(
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        // modifica stile bordi quando TextField é selezionato
+                        borderSide: BorderSide(
+                          width: 3,
+                        ),
+                      ),
+                      fillColor: utilities.mainBackgroundColor,
+                      filled: true,
+                      labelText: 'Password',
+                    ),
+                    controller: password,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      left: 20.0,
+                      right: 20.0,
+                      top: 20,
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: utilities.mainColor,
+                        padding: EdgeInsets.only(left: 50, right: 50),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                        ),
+                      ),
+                      onPressed: () {
+                        userAuthentication();
+                      },
+                      child: const Text('LOGIN',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Text("New here?",
+                        style: TextStyle(
+                          color: utilities.mainColor,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          letterSpacing: 2,
+                        )),
+                  ),
+                  const Padding(padding: EdgeInsets.only(bottom: 10)),
+                ],
+              ),
             ),
           ),
         ]),
