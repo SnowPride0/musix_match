beba49a6d6b9490cacb787e2e64b35fbcf240505
Massimiliano_Carrer <massimiliano.carrer@istitutovolterra.edu.it>
2023-01-14 09:57:56 +0000

    tolto commento application ID

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:MusixMatch/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:MusixMatch/utilities.dart' as utilities;
 
-//DA FARE: CAMBIA APPLICATION ID
-
 void main() {
   runApp(Main());
 }
